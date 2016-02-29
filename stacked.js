d378a280384a554c0bcb6c83e8403d081c3cec74
temi <temivarg@gmail.com>
2016-02-29 16:08:45 +1100

    takes care of dynamic changes to a table. It only does for table that registered with responsive table.

--- stacked.js
+++ stacked.js
@@ -16,9 +16,42 @@
  */
 $(document).ready(function() {
     $('.responsive-table-stacked').each(function(index, item){
-        $(item).find('thead th').each(function(col, th){
-            var colNum = col + 1;
-            $(item).find('tbody tr td:nth-child('+colNum+')').attr('data-th',$(th).text().trim());
-        });
+        addAttributeToTd(item)
+        watch(this, addAttributeToTd)
     });
 });
+
+/**
+ * adding data-th attribute to td elements of the table. this attribute is used to display
+ * header on each table cell.
+ * @param item
+ */
+function addAttributeToTd(item){
+    $(item).find('thead th').each(function(col, th){
+        var colNum = col + 1;
+        $(item).find('tbody tr td:nth-child('+colNum+')').attr('data-th',$(th).text().trim());
+    });
+}
+
+/**
+ * checks if the inner html has changed on the target element and calls back if true.
+ * http://stackoverflow.com/questions/12386058/html-table-onchange
+ * @param targetElement
+ * @param triggerFunction
+ * @param delay
+ */
+function watch( targetElement, triggerFunction, delay ){
+    delay = delay || 500;
+    /// store the original html to compare with later
+    var html = targetElement.innerHTML;
+    /// start our constant checking function
+    setInterval(function(){
+        /// compare the previous html with the current
+        if ( html != targetElement.innerHTML ) {
+            /// trigger our function when a change occurs
+            triggerFunction(targetElement);
+            /// update html so that this doesn't keep triggering
+            html = targetElement.innerHTML;
+        }
+    },delay);
+}
